8566480326983c53c2c494d0e809f8588259edc4
IllyasvielvonE <1296178430@qq.com>
2022-04-23 16:06:35 +0800

    Text & OS(partial) updated
    Relatively complete part at present.

---
+++ 01_Text/textwrap_example.py
@@ -0,0 +1,16 @@
+
+sample_text = '''
+    The textwrap module can be used to format text for output in
+    situations where pretty-printing is desired.  It offers
+    programmatic functionality similar to the paragraph wrapping
+    or filling features found in many text editors.
+    '''
+
+sample_elden = '''
+    If you talk several times to Roderika while she is found on Stormhill Shack, you will obtain
+Sitting Sideways pose and then she will give you the Spirit Jellyfish Ashes.
+    Deliver her message and return with a Chrystalids' Memento found on a pile of corpses within Rampart Tower. 
+She will still be at the shack. The next time you meet Roderika will be at the Roundtable Hold. Meet her there to obtain a Golden Seed.
+    She can be found later again in Roundtable Hold as a Spirit Tuner where she is not wearing her riding hood anymore, 
+provided that you have given her the Chrysalids' Memento and have spoken to Smithing Master Hewg.
+    '''
